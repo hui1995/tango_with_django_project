a9a9ca2b6488b483d9f823e2114521b1b7845fa8
“one_code@163.com” <glorycho1116@gmail.com>
2020-01-01 18:10:53 +0800

    update

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'px0i%r$0syd^d@j&g)v7xp2#zc5bqfk(ksg2&0m^l9hmzrvrm('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["http://cats.pythonanywhere.com/"]
 
